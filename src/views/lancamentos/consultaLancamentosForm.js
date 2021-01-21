f7cf3c5ebafca58eef326d146ffe82acc7b3b9bd
Luiz Evangelista <luiz.evangelista@meupag.com.br>
2021-01-21 19:32:40 -0300

    ajustando consulta lancamentos para card

--- src/views/lancamentos/consultaLancamentosForm.js
+++ src/views/lancamentos/consultaLancamentosForm.js
@@ -1,9 +1,11 @@
 import { React, useState } from 'react';
 
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import { FormControl } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
@@ -20,6 +22,13 @@ const useStyles = makeStyles((theme) => ({
   },
   formControlTextField: {
     width: '30ch',
+  },
+  cardAction: {
+    justifyContent: "center",
+  },
+  cardContent: {
+    textAlign: "center", 
+    justifyContent: "center",
   },
 }));
   
@@ -41,104 +50,98 @@ export default function ConsultaLancamentoForm({meses, tipos, buscar, cadastrar}
 
   return (
     <div className={classes.root}>
-     <form onSubmit={
+      <form onSubmit={
                 (event) => {
                     event.preventDefault();
                     buscar({ano, mes, tipo, descricao});
                 }
             }>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-        <Paper className={classes.paper}>
-        <Typography variant="h3" gutterBottom>
-          Consulta Lançamentos
-        </Typography>
-        </Paper>
-          <Paper className={classes.paper}>
-            <FormControl className={classes.formControlTextField}>
-                <TextField id="outlined-full-width-ano" label="Ano" fullWidth
-                    margin="normal"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    variant="outlined"
-                    value={ano}
-                    onChange={
-                        event => {
-                            setAno(event.target.value);
-                        }
-                    }
-                />
-              </FormControl>
-          </Paper>
-          <Paper className={classes.paper}>
-               <FormControl variant="outlined" className={classes.formControlTextField}>
-                    <Select
-                        native
-                        value={mes}
-                        onChange={
-                            event => {
-                                setMes(event.target.value);
-                            }
-                        }
-                        label="Mês"
-                        inputProps={{
-                            name: 'mes',
-                            id: 'outlined-mes-native-simple',
-                        }}
-                    >
-                    {optionsMeses}
-                    </Select>
-                </FormControl>
-          </Paper>
-          <Paper className={classes.paper}>
-            <FormControl className={classes.formControlTextField}>
-                <TextField id="outlined-full-width-descricao" label="Descrição" fullWidth
-                    margin="normal"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    variant="outlined" 
-                    value={descricao}
-                    onChange={
-                        event => {
-                            setDescricao(event.target.value);
-                        }
-                    }
-                />
-            </FormControl>
-          </Paper>
-          <Paper className={classes.paper}>
-          <FormControl variant="outlined" className={classes.formControlTextField}>
-                    <Select
-                        native
-                        value={tipo}
-                        onChange={
-                            event => {
-                                setTipo(event.target.value);
-                            }
-                        }
-                        label="Tipo"
-                        inputProps={{
-                            name: 'tipo',
-                            id: 'outlined-tipo-native-simple',
-                        }}
-                    >
-                    {optionsTipos}
-                    </Select>
-                </FormControl>
-          </Paper>
-          <Paper className={classes.paper}>
-            <Button type="submit" variant="contained" color="primary">
-              Buscar
-            </Button>
-            <Button variant="contained" color="secondary" onClick={cadastrar}>
-                Cadastrar
-            </Button>
-          </Paper>
-        </Grid>
-      </Grid>
-    </form>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                  <Card className={classes.root}>
+                      <CardContent className={classes.cardContent}>
+                          <Typography variant="h3" gutterBottom>
+                            Consulta Lançamentos
+                          </Typography>
+                          <FormControl className={classes.formControlTextField}>
+                            <TextField id="outlined-full-width-ano" label="Ano" fullWidth
+                                margin="normal"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                variant="outlined"
+                                value={ano}
+                                onChange={
+                                    event => {
+                                        setAno(event.target.value);
+                                    }
+                                }
+                            />
+                          </FormControl><br />
+                          <FormControl variant="outlined" className={classes.formControlTextField}>
+                              <Select
+                                  native
+                                  value={mes}
+                                  onChange={
+                                      event => {
+                                          setMes(event.target.value);
+                                      }
+                                  }
+                                  label="Mês"
+                                  inputProps={{
+                                      name: 'mes',
+                                      id: 'outlined-mes-native-simple',
+                                  }}
+                              >
+                              {optionsMeses}
+                              </Select>
+                          </FormControl><br/>
+                          <FormControl className={classes.formControlTextField}>
+                              <TextField id="outlined-full-width-descricao" label="Descrição" fullWidth
+                                  margin="normal"
+                                  InputLabelProps={{
+                                      shrink: true,
+                                  }}
+                                  variant="outlined" 
+                                  value={descricao}
+                                  onChange={
+                                      event => {
+                                          setDescricao(event.target.value);
+                                      }
+                                  }
+                              />
+                          </FormControl><br />
+                          <FormControl variant="outlined" className={classes.formControlTextField}>
+                              <Select
+                                  native
+                                  value={tipo}
+                                  onChange={
+                                      event => {
+                                          setTipo(event.target.value);
+                                      }
+                                  }
+                                  label="Tipo"
+                                  inputProps={{
+                                      name: 'tipo',
+                                      id: 'outlined-tipo-native-simple',
+                                  }}
+                              >
+                              {optionsTipos}
+                              </Select>
+                          </FormControl><br/>
+                      </CardContent>
+                      <CardActions className={classes.cardAction}>
+                        <Button type="submit" variant="contained" color="primary">
+                          Buscar
+                        </Button>
+                        <Button variant="contained" color="secondary" onClick={cadastrar}>
+                            Cadastrar
+                        </Button>
+                      </CardActions>
+                  </Card>
+              </Grid>
+          </Grid>
+      </form>
     </div>
   );
 }
