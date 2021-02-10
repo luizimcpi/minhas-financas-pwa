5f5b999c12e3624c09d272bd04bfb309c6fd194a
Luiz Evangelista <luiz.evangelista@meupag.com.br>
2021-02-10 12:03:37 -0300

    removendo material ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import 'fontsource-roboto'
-// import 'startbootstrap-sb-admin-2/vendor/jquery/jquery.min.js'
-// import 'startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min.js'
-// import 'startbootstrap-sb-admin-2/js/sb-admin-2.min.js'
 import 'startbootstrap-sb-admin-2/css/sb-admin-2.min.css'
 import 'startbootstrap-sb-admin-2/vendor/fontawesome-free/css/all.min.css'
 import 'startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js'
@@ -10,7 +6,6 @@ import './custom.css'
 import ProvedorAutenticacao from './provedorAutenticacao'
 import React from 'react';
 import Rotas from './rotas'
-
 
 function App() {
   
