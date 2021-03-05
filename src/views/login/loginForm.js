87679f5293b2cb4c4cc8e21e0b9343f5aa626549
Luiz Evangelista <luiz.evangelista@meupag.com.br>
2021-03-05 14:37:44 -0300

    ajustando tela login

--- src/views/login/loginForm.js
+++ src/views/login/loginForm.js
@@ -19,36 +19,36 @@ export default function LoginForm({entrar, usuarios}) {
   return (
 
        <Page noToolbar noNavbar noSwipeback loginScreen>
-  <LoginScreenTitle>Framework7</LoginScreenTitle>
-  <List form>
-    <ListInput
-      label="Username"
-      type="text"
-      placeholder="Your username"
-      value={email}
-      onInput={(e) => {
-        setEmail(e.target.value);
-      }}
-    />
-    <ListInput
-      label="Password"
-      type="password"
-      placeholder="Your password"
-      value={senha}
-      onInput={(e) => {
-        setSenha(e.target.value);
-      }}
-    />
-  </List>
-  <List>
-    <ListButton onClick={usuarios}>Sign In</ListButton>
-    <BlockFooter>
-      Some text about login information.
-      <br />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    </BlockFooter>
-  </List>
-    </Page>
+        <LoginScreenTitle>Framework7</LoginScreenTitle>
+        <List form>
+          <ListInput
+            label="Username"
+            type="text"
+            placeholder="Your username"
+            value={email}
+            onInput={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <ListInput
+            label="Password"
+            type="password"
+            placeholder="Your password"
+            value={senha}
+            onInput={(e) => {
+              setSenha(e.target.value);
+            }}
+          />
+        </List>
+        <List>
+          <ListButton onClick={usuarios}>Sign In</ListButton>
+          <BlockFooter>
+            Some text about login information.
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </BlockFooter>
+        </List>
+          </Page>
 
   );
 }
