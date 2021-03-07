6994754afc015440d77f2edcc6a0a98ea3966cab
Luiz Henrique <luizhenrique.se@gmail.com>
2021-03-07 19:50:40 -0300

    utilizando elementos do framework 7 no login

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,19 +1,15 @@
-import AlertDialogInformation from '../../components/alertDialogInformation'
-import AlertLoading from '../../components/alertLoading'
 import { AuthContext } from '../../provedorAutenticacao'
 import LoginForm from './loginForm';
 import React from 'react';
 import UsuarioService from '../../app/service/usuarioService'
 import httpStatus from 'http-status'
 import { withRouter } from 'react-router-dom'
-import { f7 } from 'framework7-react';
+import { Progressbar, f7 } from 'framework7-react'
 
 class Login extends React.Component {
 
     state = {
-        showLoadingDialog: false,
-        mensagemAlerta: '',
-        showInfoDialog: false
+        showLoadingDialog: false
     }
 
     constructor(){
@@ -24,29 +20,31 @@ class Login extends React.Component {
     aoEntrarForm = (dados) => { 
        
         if(!dados.email){
-            f7.dialog.alert('Favor preencher o campo e-mail...', () => {});
+            f7.dialog.alert('Favor preencher o campo e-mail...', () => {})
             return false
         }
 
         if(!dados.senha){
-            f7.dialog.alert('Favor preencher o campo senha...', () => {});
+            f7.dialog.alert('Favor preencher o campo senha...', () => {})
             return false
         }
-
+        
         this.setState({showLoadingDialog: true})
 
         this.service.autenticar({
             email: dados.email,
             senha: dados.senha
         }).then( response => {
-            this.setState({showLoadingDialog: false})
+            f7.progressbar.hide()
             this.context.iniciarSessao(response.data) 
             this.props.history.push('/home')
         }).catch( erro => {
             if(erro.response.status === httpStatus.FORBIDDEN) {
-                this.setState({showLoadingDialog: false, showInfoDialog: true, mensagemAlerta: 'Nome de usuário / senha inválido(s), tente novamente ou mais tarde...'})
+                this.setState({showLoadingDialog: false})
+                f7.dialog.alert('Nome de usuário / senha inválido(s), tente novamente ou mais tarde...', () => {})
             } else {
-                this.setState({showLoadingDialog: false, showInfoDialog: true, mensagemAlerta: 'Ocorreu um erro inesperado... Entre em contato com o Administrador.'})
+                this.setState({showLoadingDialog: false})
+                f7.dialog.alert('Ocorreu um erro inesperado... Entre em contato com o Administrador.', () => {})
             }
         })
     }
@@ -55,20 +53,11 @@ class Login extends React.Component {
         this.props.history.push('/cadastro-usuarios')
     }
 
-    fecharAlertaAviso = () => {
-        this.setState({showInfoDialog: false, mensagemAlerta: ''})
-    }
-
     render(){
         return(
             <React.Fragment>
+                <Progressbar infinite color="blue" style={{ display: this.state.showLoadingDialog ? 'block': 'none'}} />
                 <LoginForm entrar={this.aoEntrarForm} usuarios={this.toUsuarios}/>
-                <AlertDialogInformation 
-                    open={this.state.showInfoDialog} 
-                    close={this.fecharAlertaAviso} 
-                    mensagemCustomizada={this.state.mensagemAlerta} 
-                />     
-                <AlertLoading open={this.state.showLoadingDialog} />
             </React.Fragment>
         )
     }
