1aa590bcc8913f2cb51feba336640bd141f0a582
Luiz Henrique <luizhenrique.se@gmail.com>
2025-05-13 23:24:04 -0300

    fix: refresh page login problem

--- src/app/service/authService.js
+++ src/app/service/authService.js
@@ -1,23 +1,43 @@
 import LocalStorageService from './localStorageService'
+import jwt from 'jsonwebtoken'
 
 export const USUARIO_LOGADO = '_usuario_logado'
+export const TOKEN = 'accessToken'
 
 export default class AuthService {
 
     static isUsuarioAutenticado(){
-        const usuario = LocalStorageService.obterItem(USUARIO_LOGADO)
-        return usuario && usuario.id
+        const token = LocalStorageService.obterItem(TOKEN)
+        if(!token){
+            return false
+        }
+        const decodedToken = jwt.decode(token)
+        const expirationDate = decodedToken.exp
+   
+        const isTokenInvalido = Date.now() >= (expirationDate * 1000)
+    
+        return !isTokenInvalido
     }
 
     static removerUsuarioAutenticado(){
         LocalStorageService.removerItem(USUARIO_LOGADO)
+        LocalStorageService.removerItem(TOKEN)
     }
 
-    static logar(usuario){
+    static logar(usuario, token){
         LocalStorageService.adicionarItem(USUARIO_LOGADO, usuario)
+        LocalStorageService.adicionarItem(TOKEN, token)
     }
 
     static obterUsuarioAutenticado(){
         return LocalStorageService.obterItem(USUARIO_LOGADO)
     }
+
+    static refreshSession(){
+        const token = LocalStorageService.obterItem(TOKEN)
+        const usuario = AuthService.obterUsuarioAutenticado()
+        AuthService.logar(usuario, token)
+        return usuario
+    }
+
 }
--- src/provedorAutenticacao.js
+++ src/provedorAutenticacao.js
@@ -1,5 +1,6 @@
 import AuthService from './app/service/authService'
 import React from 'react'
+import jwt from 'jsonwebtoken'
 
 export const AuthContext = React.createContext()
 export const AuthConsumer = AuthContext.Consumer;
@@ -10,20 +11,41 @@ class ProvedorAutenticacao extends React.Component{
 
     state = {
         usuarioAutenticado: null,
-        isAutenticado: false
+        isAutenticado: false,
+        isLoading: true
     }
 
     iniciarSessao = (usuario) => {
-        AuthService.logar(usuario)
+        AuthService.logar(usuario, usuario.accessToken)
         this.setState({ isAutenticado: true, usuarioAutenticado: usuario })
     }
-
+    
     encerrarSessao = () => {
         AuthService.removerUsuarioAutenticado()
         this.setState({ isAutenticado: false, usuarioAutenticado: null })
     }
 
+    async componentDidMount(){
+        const isAutenticado = AuthService.isUsuarioAutenticado()
+        if(isAutenticado){
+            const usuario = await AuthService.refreshSession()
+            this.setState({ isAutenticado: true, usuarioAutenticado: usuario, isLoading: false })
+        } else {
+            this.setState( previousState => {  
+                return {
+                    ...previousState,
+                    isLoading: false
+                }
+            })
+        }
+    }
+
     render(){
+
+        if(this.state.isLoading){
+            return null;
+        }
+
         const contexto = {
             usuarioAutenticado: this.state.usuarioAutenticado,
             isAutenticado: this.state.isAutenticado,
